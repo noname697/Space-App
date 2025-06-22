ab6f61c56ef64a05c325e7f8c00a57067b26f293
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-22 13:58:14 -0300

    Estilo variado baseado em prop com styled-components

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
-const ItemListaEstilizado = styled.li``;
+const ItemListaEstilizado = styled.li`
+  font-size: 24px;
+  line-height: 29px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 22px;
+  color: ${(props) => (props.$ativo ? "#7B78E5" : "#D9D9D9")};
+`;
 
 const ItemNavegacao = ({
   children,
@@ -9,9 +18,9 @@ const ItemNavegacao = ({
   ativo = false,
 }) => {
   return (
-    <ItemListaEstilizado>
+    <ItemListaEstilizado $ativo={ativo}>
       <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
-      <a href="">{children}</a>
+      {children}
     </ItemListaEstilizado>
   );
 };
