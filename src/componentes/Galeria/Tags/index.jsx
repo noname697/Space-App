73fddb1991f90dc0dca8fa42ff46e4a5ba516af8
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-22 15:06:48 -0300

    Criando estrutura da galeria

---
+++ src/componentes/Galeria/Tags/index.jsx
@@ -0,0 +1,5 @@
+const Tags = () => {
+    return <p>wefew</p>
+}
+
+export default Tags
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,11 +1,28 @@
-import Titulo from "../Titulo"
+import Titulo from "../Titulo";
+import Tags from "./Tags";
+import Populares from "./Populares";
+import styled from "styled-components";
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`;
+
+const SecaoFluida = styled.section`
+  flex-grow: 1;
+`;
 
 const Galeria = () => {
-    return (
-        <>
-        <Titulo>Navegue pela galeria</Titulo>
-        </>
-    )
-}
+  return (
+    <>
+      <Tags />
+      <GaleriaContainer>
+        <SecaoFluida>
+          <Titulo>Navegue pela galeria</Titulo>
+        </SecaoFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
+  );
+};
 
-export default Galeria
+export default Galeria;
